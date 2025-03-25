7cb2b5bb4db12c4f0c74e974a1730ce1ffda6ccc
Noons-ledev <noah.briet@icloud.com>
2025-03-25 11:16:15 +0100

    fix linterfile

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
   const array = [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
-  ]
+    new ClassRoom(34),
+  ];
   return array;
 }
