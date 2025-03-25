36dc906cc82dfbcd9523e12351ff71cfcad4a41c
Noons-ledev <noah.briet@icloud.com>
2025-03-25 09:54:10 +0100

    t12

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,4 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const MyObject = { [departmentName]: employees };
-  return MyObject;
+  return { [departmentName]: [...employees] };
 }
