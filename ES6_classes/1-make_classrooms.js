1d64b220583bb890324e55a806f981b3b5c308c0
Noons-ledev <noah.briet@icloud.com>
2025-03-25 11:12:37 +0100

    fix linter

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 import ClassRoom from './0-classroom.js';
 
-export default function initializeRooms(){
+export default function initializeRooms() {
   const array = [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
   ]
   return array;
 }
