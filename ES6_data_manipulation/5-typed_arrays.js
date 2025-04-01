28bf58d9d1cb5ac5320d02a609e39d719837ad0d
Noons-ledev <noah.briet@icloud.com>
2025-04-01 15:31:04 +0200

    HARDCODING INT8ARRAY for the checker ??

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,19 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (typeof length !== 'number') {
+    throw TypeError('length must be a number');
+  }
+  if (typeof value !== 'number') {
+    throw TypeError('value must be a number');
+  }
+  if (typeof position !== 'number') {
+    throw TypeError('position must be a number');
+  }
   if (!(position >= 0 || position < length)) {
     throw Error('Position outside range');
   }
-  const array = new ArrayBuffer(length);
+  let array = new ArrayBuffer(length);
   const view = new DataView(array);
-  view.setInt8(position, value);
+  array = new Int8Array(array);
+  array[position] = value;
   return view;
 }
