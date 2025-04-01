13e1c6e554904ee08dda04e7b45af0d417a455f6
Noons-ledev <noah.briet@icloud.com>
2025-04-01 15:26:54 +0200

    fix ?

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,4 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (typeof length !== 'number') {
-    throw TypeError('length must be a number');
-  }
-  if (typeof value !== 'number') {
-    throw TypeError('value must be a number');
-  }
-  if (typeof position !== 'number') {
-    throw TypeError('position must be a number');
-  }
   if (!(position >= 0 || position < length)) {
     throw Error('Position outside range');
   }
