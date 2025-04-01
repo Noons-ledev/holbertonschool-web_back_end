c5ea35ea57481a8b15bca76c33fd79200229066a
Noons-ledev <noah.briet@icloud.com>
2025-04-01 22:41:44 +0200

    t8 done, forEach usage

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,21 @@
+export default function cleanSet(set, startString) {
+  if (!(set instanceof Set)) {
+    throw TypeError('set must be a Set');
+  }
+  if (typeof startString !== 'string') {
+    throw TypeError('startString must be a string');
+  }
+  if (startString.length === 0) {
+    return '';
+  }
+  let MyString = '';
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      MyString += value.replace(startString, '');
+      MyString += '-';
+    }
+    return MyString;
+  });
+  MyString = MyString.slice(0, -1);
+  return MyString;
+}
