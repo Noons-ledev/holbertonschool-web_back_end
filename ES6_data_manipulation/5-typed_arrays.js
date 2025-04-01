68b41869ac2619c38cc11b0167cc94a35216cade
Noons-ledev <noah.briet@icloud.com>
2025-04-01 15:22:56 +0200

    Typed array

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -8,6 +8,9 @@ export default function createInt8TypedArray(length, position, value) {
   if (typeof position !== 'number') {
     throw TypeError('position must be a number');
   }
+  if (!(position >= 0 || position < length)) {
+    throw Error('Position outside range');
+  }
   const array = new ArrayBuffer(length);
   const view = new DataView(array);
   view.setInt8(position, value);
