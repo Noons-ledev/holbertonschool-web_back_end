60a812fb9d9135b98a4436c8882c9ac3bdcdc134
Noons-ledev <noah.briet@icloud.com>
2025-04-02 13:49:49 +0200

    t4 Promise.resolve and fix t3 for return Promise.all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import {uploadPhoto, createUser} from "./utils.js";
 export default function handleProfileSignup() {
   const p = uploadPhoto();
   const p1 = createUser();
-  Promise.all([p, p1])
+  return Promise.all([p, p1])
     .then(([photo, user]) => {
       const result = `${photo.body} ${user.firstName} ${user.lastName}`;
       console.log(result);
