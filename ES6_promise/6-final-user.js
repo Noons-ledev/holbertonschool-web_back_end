65b5e383f31aa7e0ee45ee01d8b74f5b5083c2d2
Noons-ledev <noah.briet@icloud.com>
2025-04-02 14:44:27 +0200

    async try ?

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,9 +2,9 @@
 import signUpUser from "./4-user-promise.js";
 // eslint-disable-next-line
 import uploadPhoto from "./5-photo-reject";
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default async function handleProfileSignup(firstName, lastName, filename) {
   const p1 = signUpUser(firstName, lastName);
   const p2 = uploadPhoto(filename);
-  const result = Promise.allSettled([p1, p2]);
+  const result = await Promise.allSettled([p1, p2]);
   return result;
 }
