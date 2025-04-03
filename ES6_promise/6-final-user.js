faeb42c21a861b09b0828d6142743e1043770570
Noons-ledev <noah.briet@icloud.com>
2025-04-03 11:25:36 +0200

    Ouah

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line
-import signUpUser from "./4-user-promise.js";
+import printf from "./4-user-promise.js";
 // eslint-disable-next-line
 import uploadPhoto from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+  return Promise.allSettled([printf(firstName, lastName), uploadPhoto(filename)])
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fullfilled' ? result.value : result.reason.toString(),
