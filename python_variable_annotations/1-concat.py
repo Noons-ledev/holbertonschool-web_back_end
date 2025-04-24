665e087a35afc5e7c91ab80663fd18675f4caa62
Noons-ledev <noah.briet@icloud.com>
2025-04-24 15:25:31 +0200

    t1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module doc here
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings
+    """
+    String: str = "{}{}".format(str1, str2)
+    return String
