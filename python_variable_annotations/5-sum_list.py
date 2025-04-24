6213c00ade83c9d3522b8aa2c3ce99f298d116e7
Noons-ledev <noah.briet@icloud.com>
2025-04-24 15:49:48 +0200

    t5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module doc here
+"""
+
+
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """
+    Function doc
+    """
+    return sum(input_list)
