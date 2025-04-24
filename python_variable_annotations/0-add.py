10f3b0b8fcb1b5eee7226636663bb23af3d67609
Noons-ledev <noah.briet@icloud.com>
2025-04-24 15:22:08 +0200

    P1 T0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+"""
+Module used to implement adding method
+"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats
+    """
     return a + b
