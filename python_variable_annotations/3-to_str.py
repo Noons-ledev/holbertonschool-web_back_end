c589572f47ee3f78f59d4efe4d3150e2860c5aa6
Noons-ledev <noah.briet@icloud.com>
2025-04-24 15:41:28 +0200

    t3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module doc here
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns string representation of an object
+    """
+    string: str = "{}".format(n)
+    return string
