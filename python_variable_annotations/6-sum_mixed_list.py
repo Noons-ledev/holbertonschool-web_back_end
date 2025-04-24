482bb6186f07f9a0b0e537ef5c7ab7d8367b4cfa
Noons-ledev <noah.briet@icloud.com>
2025-04-24 15:52:53 +0200

    t6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module doc here
+"""
+import typing
+
+
+def sum_mixed_list(mxd_list: typing.List[typing.Union[int, float]]) -> float:
+    """
+    method documentation here
+    """
+    return sum(mxd_list)
