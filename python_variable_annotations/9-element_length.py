45f4b64e6c7c03a7604bea2538585b4cdfc50eb9
Noons-ledev <noah.briet@icloud.com>
2025-04-25 00:10:41 +0200

    last tasks

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,8 +2,9 @@
 """
 Module doc here
 """
-import typing
+from typing import List, Iterable, Sequence, Tuple
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples"""
     return [(i, len(i)) for i in lst]
