d64cd4eb0604ee33a707dfada076b205d659e935
Noons-ledev <noah.briet@icloud.com>
2025-04-28 11:12:55 +0200

    Type annotating my function with typing.asyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Module doc here
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Function doc here
     """
