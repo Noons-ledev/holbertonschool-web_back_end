950fc82554b0e50f24be9bc40cece387347f05b2
Noons-ledev <noah.briet@icloud.com>
2025-04-28 11:27:19 +0200

    forgot to type annotate

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,7 @@ import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> typing.List[float]:
     """Function doc here"""
     result = [x async for x in async_generator()]
     return result
