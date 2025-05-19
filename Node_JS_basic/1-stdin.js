e948f13e9574776f5c71bbed880a675e41cb2085
Noons-ledev <noah.briet@icloud.com>
2025-05-19 04:32:19 +0200

    t1 using process + destroy ? Nah, pause again

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
-  process.stdin.destroy();
+  process.stdin.pause();
 });
 
 process.stdin.on('end', () => {
