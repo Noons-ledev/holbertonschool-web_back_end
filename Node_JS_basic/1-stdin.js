574056d495864ce055e200e243f2f9b7c889b23d
Noons-ledev <noah.briet@icloud.com>
2025-05-21 15:51:38 +0200

    removing trim() function

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
+  const name = data;
+  process.stdout.write(`Your name is: ${name}`);
   process.stdin.pause();
   if (process.stdin.isTTY) {
     process.exit(0);
